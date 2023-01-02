f62d01db684bec35d799177c4c59a61b26e5e832
foreknowledge <kyj_0129@naver.com>
2023-01-03 01:34:48 +0900

    feat: 로그아웃 기능 추가

--- src/todo/TodoMain.tsx
+++ src/todo/TodoMain.tsx
@@ -1,3 +1,4 @@
+import { LOCAL_STORAGE_USER_TOKEN_KEY } from '../constants';
 import useNeedLogin from '../hook/useNeedLogin';
 import TodoItem from './TodoItem';
 import TodoList from './TodoList';
@@ -6,13 +7,29 @@ const TodoMain = () => {
   // 로그인 여부 확인
   useNeedLogin();
 
+  const handleLogout = () => {
+    const result = window.confirm('정말 로그아웃 하시겠습니까?');
+    if (!result) return;
+
+    window.localStorage.removeItem(LOCAL_STORAGE_USER_TOKEN_KEY);
+    window.location.reload();
+  };
+
   return (
-    <div className="flex h-full w-[50em]">
-      <div className="w-1/2 bg-yellow-50">
-        <TodoList />
-      </div>
-      <div className="w-1/2 bg-red-50">
-        <TodoItem />
+    <div className="flex h-full flex-col">
+      <button
+        className="focus:shadow-outline my-2 ml-auto block rounded bg-purple-500 py-2 px-4 font-bold text-white shadow focus:outline-none enabled:hover:bg-purple-400 disabled:bg-gray-400"
+        onClick={handleLogout}
+      >
+        로그 아웃
+      </button>
+      <div className="flex w-[50em] flex-1">
+        <div className="w-1/2 bg-yellow-50">
+          <TodoList />
+        </div>
+        <div className="w-1/2 bg-red-50">
+          <TodoItem />
+        </div>
       </div>
     </div>
   );
