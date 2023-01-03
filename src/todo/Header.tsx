727963e0f3038a12a1564094d4f7a0bfd639729a
foreknowledge <kyj_0129@naver.com>
2023-01-03 15:34:06 +0900

    refactor: 메인 페이지 Header 컴포넌트로 분리

---
+++ src/todo/Header.tsx
@@ -0,0 +1,22 @@
+import { LOCAL_STORAGE_USER_TOKEN_KEY } from '../constants';
+
+const Header = () => {
+  const handleLogout = () => {
+    const result = window.confirm('정말 로그아웃 하시겠습니까?');
+    if (!result) return;
+
+    window.localStorage.removeItem(LOCAL_STORAGE_USER_TOKEN_KEY);
+    window.location.reload();
+  };
+
+  return (
+    <button
+      className="focus:shadow-outline my-2 ml-auto block rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
+      onClick={handleLogout}
+    >
+      로그 아웃
+    </button>
+  );
+};
+
+export default Header;
--- src/todo/TodoMain.tsx
+++ src/todo/TodoMain.tsx
@@ -1,6 +1,6 @@
 import { useSearchParams } from 'react-router-dom';
-import { LOCAL_STORAGE_USER_TOKEN_KEY } from '../constants';
 import useNeedLogin from '../hook/useNeedLogin';
+import Header from './Header';
 import TodoDetail from './TodoDetail';
 import TodoList from './TodoList';
 
@@ -11,23 +11,10 @@ const TodoMain = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const selectedId = searchParams.get('id');
 
-  const handleLogout = () => {
-    const result = window.confirm('정말 로그아웃 하시겠습니까?');
-    if (!result) return;
-
-    window.localStorage.removeItem(LOCAL_STORAGE_USER_TOKEN_KEY);
-    window.location.reload();
-  };
-
   return (
-    <div className="flex h-full flex-col">
-      <button
-        className="focus:shadow-outline my-2 ml-auto block rounded bg-purple-500 py-2 px-4 font-bold text-white shadow focus:outline-none enabled:hover:bg-purple-400 disabled:bg-gray-400"
-        onClick={handleLogout}
-      >
-        로그 아웃
-      </button>
-      <div className="flex w-[50em] flex-1">
+    <div className="flex h-full w-[50em] flex-col">
+      <Header />
+      <div className="flex flex-1">
         <div className="w-1/2 bg-yellow-50">
           <TodoList
             selectedId={selectedId}
